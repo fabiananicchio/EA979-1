32cf8a6889d1db10802f45679315b58e87602b85
gshima <gabrielashima@gmail.com>
2019-06-20 18:08:00 -0300

    adicionado a funçao print_model no multiprocesso

--- multiprocess.py
+++ multiprocess.py
@@ -8,6 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 #from tkinter import TclError
+import pptk
+import numpy as np
+import plyfile
 
 
 def print_cube(num, vector, lock):
@@ -66,7 +69,7 @@ def plot_graph(threadName, lock, chunk_shared, is_playing_audio):
    lock.release()
    plt.show()
    while is_playing_audio == 1:
-      print(is_playing_audio)
+      print(is_p laying_audio)
 
 
 def process_data(threadName, lock, chunk_shared, is_playing_audio):
@@ -99,6 +102,111 @@ def process_data(threadName, lock, chunk_shared, is_playing_audio):
    stream.close()
    p.terminate
 
+def print_model():
+   data = plyfile.PlyData.read('g.ply')['vertex']
+
+   xyz = np.c_[data['x'], data['y'], data['z']]          # Eixos
+   rgb = np.c_[data['red'], data['green'], data['blue']] # RGB
+   n = np.c_[data['nx'], data['ny'], data['nz']]         # Normais
+
+   # v = pptk.viewer(xyz)
+   # v.set(point_size=0.005)
+   # v.attributes(rgb / 255., 0.5 * (1 + n))
+
+   # extractedData = rgb[:,[2]]  # gets x axis
+
+   # matrix = 1*[1*[256]]
+   # print(matrix)
+   # rgb[:,[2]][0] = matrix
+   # print(rgb[:,[2]])
+
+   P = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9],
+              [10, 11, 12]])
+
+   print("\nP Array:")
+   print(P)
+
+   print("\nP[0] Array: selects first row")
+   print(P[0])
+
+   print("\nP[:, 0] Array: selects column")
+   print(P[:, 0])
+
+   P[0] = [0, 0, 0]
+   print("\nP[0] Modified Array")
+   print(P[0])
+
+   P[:, 0][0] = 12345678
+   print("\nP[:,1] Modified Array")
+   print(P)
+
+   print(type(P))
+
+   print("-------------- model rbg array --------------")
+
+   print("RGB Array:")
+   print(rgb)
+
+   print("\nRGB[0] Array: selects first row")
+   print(rgb[0])
+
+   print("\nP[:, 1] Array: selects green column")
+   print(rgb[:, 1])
+
+   rgb[0] = 0
+   print("\nRGB[0] Modified Array")
+   print(rgb[0])
+
+   # rgb[:, 1] = 0
+   print("\nRGB[:,1] Modified Array")
+   print(rgb)
+   print(type(rgb))
+
+
+   # size = len(rgb[:,[2]])
+   # for x in range(0, size):
+   #     rgb[:,[2]][x] = 255;
+
+
+   # Setup viewer
+   v = pptk.viewer(xyz)
+   v.set(point_size = 0.007)
+   v.attributes(rgb / 255., 0.5 * (1 + n))
+   v.set(bg_color = [0, 0, 0, 1])
+   v.set(floor_color = [0, 0, 0, 1])
+   v.set(show_grid = False)
+   v.set(show_axis = False)
+   v.set(lookat = [-0.5, 5.50, -8])
+   v.set(theta = 0.6)
+   v.set(phi = 6)
+   v.set(r = 20)
+
+   v.get('view')
+
+   #  (x, y, z, phi, theta, r)
+   # Rotates camera
+   poses = []
+   poses.append([-10, 5, 0, 6 , 0.6, 5])
+   poses.append([-5, 5, 0, 6, 0.6, 5])
+   poses.append([0, 5, 0, 6, 0.6, 5])
+   poses.append([5, 5, 0, 6, 0.6, 5])
+   poses.append([10, 5, 0, 6, 0.6, 5])
+   poses.append([12, 5, 0, 6, 0.6, 5])
+   # play(poses, ts=[], tlim=[-inf, inf], repeat=False, interp='cubic_natural')
+   v.play(poses, 2 * np.arange(len(poses)), repeat=True, interp='linear')
+
+   print(type(v))
+
+   #
+
+   # for x in extractedData
+   #     element = element % 256 * 1.5
+
+
+   # print(extractedData)
+
 if __name__ == "__main__":
     lock = multiprocessing.Lock()
     #creating shared memory
@@ -108,7 +216,7 @@ if __name__ == "__main__":
 
     # creating processes
     p1 = multiprocessing.Process(target=print_cube, args=(10, vector, lock))
-    p2 = multiprocessing.Process(target=print_square, args=(10, vector, lock))
+    p2 = multiprocessing.Process(target=print_model, args=( ))
     p3 = multiprocessing.Process(target=print_time, args=("Thread-3",2))
     p4 = multiprocessing.Process(target=play_sound, args=("Thread-4",2, chunk_shared, lock, is_playing_audio))
     p5 = multiprocessing.Process(target=plot_graph, args=("Thread-5", lock, chunk_shared, is_playing_audio))
